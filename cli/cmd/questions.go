91a3ade524737c97d43b6f451e78b39f0c1f7e7f
Giancarlo Laferla <gianlaf@gmail.com>
2022-05-29 22:42:52 +0200

    basic implementation of getQuestions/data in cli

--- cli/cmd/questions.go
+++ cli/cmd/questions.go
@@ -5,7 +5,10 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 
 	"github.com/spf13/cobra"
 )
@@ -16,7 +19,7 @@ var questionsCmd = &cobra.Command{
 	Short: "This command is used to view all the questions in the quiz",
 	Long:  `This command is used to view all the questions in the quiz`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("questions called")
+		getQuestions()
 	},
 }
 
@@ -32,4 +35,54 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// questionsCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+type question struct {
+	ID            int       `json:"id"`
+	Question      string    `json:"question"`
+	AllAnswers    [4]string `json:"allAnswers"`
+	CorrectAnswer int       `json:"correctAnswer"`
+}
+
+func getQuestions() {
+	url := "localhost:8080/quiz"
+	responseBytes := getQuestionsData(url)
+	allQuestions := []question{}
+
+	if err := json.Unmarshal(responseBytes, &allQuestions); err != nil {
+		fmt.Printf("Could not unmarshal response - %w", err)
+	}
+
+	for _, question := range allQuestions {
+		fmt.Println(question.Question)
+		for i, answer := range question.AllAnswers {
+			fmt.Printf("%d) %s\n", i, answer)
+		}
+		fmt.Println("------------------------------------")
+	}
+}
+
+func getQuestionsData(baseApi string) []byte {
+	request, err := http.NewRequest(
+		http.MethodGet,
+		baseApi,
+		nil,
+	)
+	if err != nil {
+		fmt.Printf("Could not request the questions - %w", err)
+	}
+
+	request.Header.Add("Accept", "application/json")
+
+	response, err := http.DefaultClient.Do(request)
+	if err != nil {
+		fmt.Printf("Could not request the questions - %w", err)
+	}
+
+	responseBytes, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Printf("Could not read body - %w", err)
+	}
+
+	return responseBytes
 }
--- cli/main.go
+++ cli/main.go
@@ -4,7 +4,7 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "quizCli/QuizCli/cmd"
+import "QuizTest/cli/cmd"
 
 func main() {
 	cmd.Execute()
